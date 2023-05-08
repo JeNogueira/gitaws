c249ba10119e8e9bd211b7fcd76d16db57b0a54f
J Nogueira <128533741+JeNogueira@users.noreply.github.com>
2023-05-08 15:31:53 -0300

    index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('<h1>Bem-vindo à página inicial - Matheus Frota - Jefferson Nogueira - site do repositorio git</h1>');
+  res.send('<h1>Bem-vindo à página inicial - Matheus Frota - Jefferson Nogueira - site do repositorio git(modificado1)</h1>');
 });
 
 app.get('/sobre', (req, res) => {
